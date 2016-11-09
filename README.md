f96001a423ee0173b6f36735f1a30cd80848c860
Colby Olson <me@colbyolson.com>
2016-11-09 11:30:15 -0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# NTP Cookbook
+This will configure the NTP service.
+
+## Requirements
+### Platforms
+TODO
+
+### Chef
+- Chef '>= 12.5'
+
+### Cookbooks
+No other dependencies
+
+## Attributes
+TODO
+
+## Usage
+TODO
---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name 'cop_ntp'
+maintainer 'Copious Inc.'
+maintainer_email 'engineering@copiousinc.com'
+license 'MIT'
+description 'Installs and configures NTP.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.0.1'
+
+source_url 'https://github.com/copious-cookbooks/ntp'
+issues_url 'https://github.com/copious-cookbooks/ntp/issues'
+
+# supports 'ubuntu'
+# supports 'debian'
+# supports 'centos'
