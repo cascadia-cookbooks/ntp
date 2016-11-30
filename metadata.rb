a055a6a83761843850c2cc5dd9ec8bb7f6efc69a
Colby Olson <me@colbyolson.com>
2016-11-30 13:02:37 -0800

    bump to 2.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@copiousinc.com'
 license 'MIT'
 description 'Installs and configures NTP.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.1'
+version '2.0.0'
 
 source_url 'https://github.com/copious-cookbooks/ntp'
 issues_url 'https://github.com/copious-cookbooks/ntp/issues'
