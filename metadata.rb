7b8025b0953d0231b9f54b7a0b5468182b46182a
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-05-18 16:35:19 -0700

    Update supported versions

--- metadata.rb
+++ metadata.rb
@@ -9,8 +9,9 @@ version '2.0.2'
 source_url 'https://github.com/copious-cookbooks/ntp'
 issues_url 'https://github.com/copious-cookbooks/ntp/issues'
 
-supports 'ubuntu'
-supports 'debian'
-supports 'centos'
+supports 'ubuntu', '>=14.04'
+supports 'debian', '>=7'
+supports 'centos', '>=7'
+supports 'rhel', '>=7'
 
 depends 'cop_base'
